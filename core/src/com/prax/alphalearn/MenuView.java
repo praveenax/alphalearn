8c39c71ce0e10fbc17e522ca842d6ff899b22ce7
praveenax <xav.axp@gmail.com>
2017-06-04 19:22:00 +0530

    latest added files

---
+++ core/src/com/prax/alphalearn/MenuView.java
@@ -0,0 +1,34 @@
+package com.prax.alphalearn;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/**
+ * Created by praveenax on 4/6/17.
+ *
+ * to show menu screen in the game  - play and exit for now
+ *
+ *
+ *
+ */
+
+public class MenuView  extends ApplicationAdapter {
+
+    @Override
+    public void create () {
+
+    }
+
+    @Override
+    public void render () {
+
+    }
+
+    @Override
+    public void dispose () {
+
+    }
+
+
+
+
+}
--- core/src/com/prax/alphalearn/MyGdxGame2.java
+++ core/src/com/prax/alphalearn/MyGdxGame2.java
@@ -7,9 +7,15 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
-public class MyGdxGame extends ApplicationAdapter {
+//BACKUP - for stable code
+public class MyGdxGame2 extends ApplicationAdapter {
 //	SpriteBatch batch;
 //	Texture img;
 
